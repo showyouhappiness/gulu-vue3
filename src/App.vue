1e321fc1e5bde05fd2f1e3086564e024ad1ccacb
liyongjun <1477676296@qq.com>
2022-04-25 19:45:13 +0800

    初始化项目 并对其进行小改动

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png"/>
+  <Test/>
+</template>
+
+<script>
+import Test from './components/Test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Test
+  }
+}
+</script>
