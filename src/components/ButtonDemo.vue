825f0612c754d778e383537aeccdaf08c04e223f
liyongjun <1477676296@qq.com>
2022-04-26 13:38:42 +0800

    初步创建一个button按钮

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    Button 示例
-  </div>
+  <Button>你好</Button>
 </template>
+
+<script>
+import Button from '../lib/Button.vue';
+
+export default {
+  components: {
+    Button,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
