c60afcce406a0bafd7c427510e044203c9e49662
liyongjun <1477676296@qq.com>
2022-04-29 17:10:03 +0800

    button修改以及Dialog功能完成

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -174,7 +174,7 @@ $grey: grey;
     height: 14px;
     display: inline-block;
     margin-right: 4px;
-    border-radius: 8px;
+    border-radius: 8px; 
     border-color: $blue $blue $blue transparent;
     border-style: solid;
     border-width: 2px;
@@ -182,7 +182,7 @@ $grey: grey;
   }
 }
 @keyframes gulu-spin {
-  0%{transform: rotate(0deg)}
-  100%{transform: rotate(360deg)}
+  0%{transform: rotate(0deg)} 
+  100%{transform: rotate(360deg)} 
 }
 </style>
