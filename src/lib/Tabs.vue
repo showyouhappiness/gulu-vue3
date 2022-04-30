7b0c156a86c1f50795eedce17e92a2181be8f07b
liyongjun <1477676296@qq.com>
2022-04-30 12:45:33 +0800

    新增tabs disabled功能

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,13 +1,18 @@
 <template>
-<div class="gulu-tabs">
-  <div class="gulu-tabs-nav" ref="container">
-    <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
-    <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav" ref="container">
+      <div class="gulu-tabs-nav-item" v-for="(pageMessage,index) in pageMessages"
+           :ref="el => { if (pageMessage.props.title===selected) selectedItem = el }"
+           @click="select(pageMessage)"
+           :class="{selected: pageMessage.props.title=== selected ,disabled: pageMessage.props.disabled ===''}"
+           :key="index">{{ pageMessage.props.title }}
+      </div>
+      <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component :is="current" :key="current.props.title"/>
+    </div>
   </div>
-  <div class="gulu-tabs-content">
-    <component :is="current" :key="current.props.title" />
-  </div>
-</div>
 </template>
 
 <script lang="ts">
@@ -18,16 +23,22 @@ import {
   watchEffect,
   onMounted
 } from 'vue'
+
 export default {
   props: {
     selected: {
       type: String
-    }
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
   },
   setup(props, context) {
-    const selectedItem = ref < HTMLDivElement > (null)
-    const indicator = ref < HTMLDivElement > (null)
-    const container = ref < HTMLDivElement > (null)
+    const selectedItem = ref<HTMLDivElement>(null)
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
 
     onMounted(() => {
       watchEffect(() => {
@@ -46,29 +57,31 @@ export default {
       })
     })
 
-    const defaults = context.slots.default()
-    defaults.forEach((tag) => {
+    const pageMessages = context.slots.default()  // 获取默认的slot,从这里可以获取到页面里面的信息
+    pageMessages.forEach((tag) => {
       if (tag.type !== Tab) {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
     const current = computed(() => {
-      return defaults.find(tag => tag.props.title === props.selected)
+      return pageMessages.find(tag => tag.props.title === props.selected)
     })
-    const titles = defaults.map((tag) => {
-      return tag.props.title
-    })
-    const select = (title: string) => {
-      context.emit('update:selected', title)
-    }
+    // 处理点击事件，当有disabled属性时不更新选中结点，否则选中点击结点
+    const select = (pageMessage) => {
+      if (pageMessage.props.disabled === "") {
+        return;
+      }
+      context.emit("update:selected", pageMessage.props.title);
+    };
+
+
     return {
       current,
-      defaults,
-      titles,
+      pageMessages,
       select,
       selectedItem,
       indicator,
-      container
+      container,
     }
   }
 }
@@ -90,6 +103,11 @@ $border-color: #d9d9d9;
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
+
+      &.disabled {
+        color: #ccc;
+        cursor: not-allowed;
+      }
 
       &:first-child {
         margin-left: 0;
