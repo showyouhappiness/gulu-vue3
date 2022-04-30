2883174d85180a38162c840ac7a0b477607badb8
liyongjun <1477676296@qq.com>
2022-04-30 10:28:09 +0800

    修改目录结构

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import { createWebHashHistory, createRouter } from "vue-router";
 import Home from "../views/Home.vue";
 import Doc from "../views/Doc.vue";
-import SwitchDemo from "../components/SwitchDemo.vue";
-import ButtonDemo from "../components/ButtonDemo.vue";
-import DialogDemo from "../components/DialogDemo.vue";
-import TabsDemo from "../components/TabsDemo.vue";
+import SwitchDemo from "../components/Switch/SwitchDemo.vue";
+import ButtonDemo from "../components/Button/ButtonDemo.vue";
+import DialogDemo from "../components/Dialog/DialogDemo.vue";
+import TabsDemo from "../components/Tabs/TabsDemo.vue";
 
 import { h } from 'vue';
 import Markdown from '../components/Markdown.vue';
